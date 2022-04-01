cd4cb6c288f16ac641f99becd7ad4bc4314089b4
Jacob Brewer <Jacobbrewer2000@outlook.com>
2022-04-01 21:22:21 +0100

    Adding in the ip address's to the logging

--- moneypot.go
+++ moneypot.go
@@ -5,7 +5,9 @@ import (
 	"github.com/gorilla/mux"
 	"html/template"
 	"log"
+	"net"
 	"net/http"
+	"os"
 )
 
 var templates *template.Template
@@ -15,6 +17,31 @@ func init() {
 	//log.SetPrefix("LOG: ")
 	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Lshortfile)
 	log.Println("logging initialized")
+}
+
+func getIpAddress() (string, string) {
+	name, err := os.Hostname()
+	if err != nil {
+		log.Println(err)
+		return "", ""
+	}
+
+	addrs, err := net.LookupIP(name)
+	if err != nil {
+		log.Println(err)
+		return "", ""
+	}
+
+	var i4, i6 string
+	for _, a := range addrs {
+		if ipv6 := a.To16(); ipv6 != nil {
+			i6 = ipv6.String()
+		}
+		if ipv4 := a.To4(); ipv4 != nil {
+			i4 = ipv4.String()
+		}
+	}
+	return i4, i6
 }
 
 func main() {
@@ -28,7 +55,9 @@ func main() {
 	r.HandleFunc("/", home)
 
 	http.Handle("/", r)
-	if err := http.ListenAndServe("localhost:8080", nil); err != nil {
+	ip4, ip6 := getIpAddress()
+	log.Printf("listening at:\nIPV4: %v:8080\nIPV6: %v:8080", ip4, ip6)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
