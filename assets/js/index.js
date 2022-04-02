70b767c0d5fdd7271d416f5b0557944f32661548
Jacob Brewer <Jacobbrewer2000@outlook.com>
2022-04-02 19:49:17 +0100

    Adding in the withdraw function on the webpage

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,70 @@
-// Get the modal
+// Get the deposit modal
 var depositModal = document.getElementById("depositModal");
 
 // Get the button that opens the modal
 var depositBtn = document.getElementById("depositButton");
 
+// Get the withdraw modal
+var withdrawModal = document.getElementById("withdrawModal");
+
+// Get the button that opens the modal
+var withdrawBtn = document.getElementById("withdrawButton");
+
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close");
 
 // When the user clicks the button, open the modal
 depositBtn.onclick = function () {
     depositModal.style.display = "block";
 }
 
+withdrawBtn.onclick = function () {
+    withdrawModal.style.display = "block";
+}
+
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    depositModal.style.display = "none";
+// iterate through each span element for each modal
+for (var i = 0; i < span.length; i++){
+    span[i].onclick = function(){
+        depositModal.style.display = "none";
+        withdrawModal.style.display = "none";
+    }
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-    if (event.target == depositModal) {
+    if (event.target == depositModal || event.target == withdrawModal) {
         depositModal.style.display = "none";
+        withdrawModal.style.display = "none";
     }
+}
+
+function withdrawMoneyToDb(evt) {
+    evt.preventDefault();
+    let form = evt.target;
+    let formData = new FormData(form);
+    let amount = formData.get("withdrawMoneyInput");
+    console.log("amount received: " + amount)
+    if (amount != null && amount != "") {
+        $.ajax({
+            url: '/withdrawMoney',
+            method: 'post',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: () => {
+                console.log("Amount withdrawn");
+                form.reset();
+                withdrawModal.style.display = "none"
+            },
+            error: (d) => {
+                console.log("An error occurred. Please try again");
+                console.log(d)
+                form.reset();
+            }
+        });
+    }
+    return false
 }
 
 function depositMoneyToDb(evt) {
